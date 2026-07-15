ff5955138eb87b34418f38c92d796d1c47f2cdea
Nathan-E-White <nwhitemizzou@gmail.com>
2026-07-14 21:14:11 -0500

    fix: use immutable GitHub release manifests

--- internal/bundle/bundle.go
+++ internal/bundle/bundle.go
@@ -8,10 +8,10 @@ type Component struct {
 }
 
 var Components = []Component{
-	{Name: "pipelines", Version: "v1.14.0", Namespace: "tekton-pipelines", Manifest: "https://storage.googleapis.com/tekton-releases/pipeline/previous/v1.14.0/release.yaml"},
-	{Name: "triggers", Version: "v0.36.0", Namespace: "tekton-pipelines", Manifest: "https://storage.googleapis.com/tekton-releases/triggers/previous/v0.36.0/release.yaml"},
-	{Name: "chains", Version: "v0.28.0", Namespace: "tekton-chains", Manifest: "https://storage.googleapis.com/tekton-releases/chains/previous/v0.28.0/release.yaml"},
-	{Name: "results", Version: "v0.19.0", Namespace: "tekton-pipelines", Manifest: "https://storage.googleapis.com/tekton-releases/results/previous/v0.19.0/release.yaml"},
+	{Name: "pipelines", Version: "v1.14.0", Namespace: "tekton-pipelines", Manifest: "https://github.com/tektoncd/pipeline/releases/download/v1.14.0/release.yaml"},
+	{Name: "triggers", Version: "v0.36.0", Namespace: "tekton-pipelines", Manifest: "https://github.com/tektoncd/triggers/releases/download/v0.36.0/release.yaml"},
+	{Name: "chains", Version: "v0.28.0", Namespace: "tekton-chains", Manifest: "https://github.com/tektoncd/chains/releases/download/v0.28.0/release.yaml"},
+	{Name: "results", Version: "v0.19.0", Namespace: "tekton-pipelines", Manifest: "https://github.com/tektoncd/results/releases/download/v0.19.0/release.yaml"},
 	{Name: "pipelines-as-code", Version: "v0.49.0", Namespace: "pipelines-as-code", Manifest: "https://github.com/openshift-pipelines/pipelines-as-code/releases/download/v0.49.0/release.k8s.yaml"},
 }
 
